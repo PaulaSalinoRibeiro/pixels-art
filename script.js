0961b14cef0998341c865d5f193f0645649e3594
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-09 16:47:12 -0300

    define a cor preta como preferencia

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 
+
+// Defined initial color with black;
+let pixelBlack = document.querySelector('.selected')
+localStorage.setItem('bgPixelBlack', 'black');
+let bgPixelBlack = localStorage.getItem('bgPixelBlack');
+pixelBlack.style.backgroundColor = bgPixelBlack;
+
