f8be91729c45a86b686971b0e055cdcf6eb70d8e
PAULA RIBEIRO <paulasalinor@gmail.com>
2021-12-09 18:03:01 -0300

    seleciona cor da paleta

--- script.js
+++ script.js
@@ -1,8 +1,22 @@
 
 
-// Defined initial color with black;
+// Defined preference initial with color black;
 let pixelBlack = document.querySelector('.selected')
 localStorage.setItem('bgPixelBlack', 'black');
 let bgPixelBlack = localStorage.getItem('bgPixelBlack');
 pixelBlack.style.backgroundColor = bgPixelBlack;
+
+// Choose color 
+
+let chooseColor = document.querySelector('#color-palette');
+chooseColor.addEventListener('click', pickColor);
+function pickColor(event){
+    let color = document.querySelectorAll('.color');
+    for(let index = 0; index < color.length; index += 1){
+        if(color[index].className === 'color selected'){
+            color[index].className = 'color'
+        }
+    }
+    event.target.className = 'color selected'
+} 
 
